7e0095b9663f13def417e4a85e0e1d5ea75f7eea
valentinitnelav <stefan.valentin87@yahoo.com>
2018-04-18 15:42:04 +0200

    adapt to new datset

--- scripts/ro_bipartite_network.r
+++ scripts/ro_bipartite_network.r
@@ -7,28 +7,31 @@
 
 library(bipartite)
 
-# Load customized visweb plotting function.
+# Load customized web plotting function.
 # It has some additional flexibility to adjust graphical parameters.
-source("https://raw.githubusercontent.com/valentinitnelav/bipartite_webs/master/R/visweb.R")
+source("https://raw.githubusercontent.com/valentinitnelav/bipartite_webs/master/R/plot_grid.R")
 
 
 # Read & prepare data -----------------------------------------------------
 
-network <- read.csv("data/Rom_Trans_cor.csv")
+# Check first for duplicates in names using the script network_check_double_naming.r
+
+network <- read.csv("data/Rom_Trans_cor_new.csv")
 
 # Prepare matrix of interactions
 network2 <- network[, -1]
 rownames(network2) <- network[, 1]
 # In column names (pollinators), substitute dot/multiple dots with space,
-# so that the dots do not trickle down to the labels in the graph.
-colnames(network2) <- gsub(pattern = "[.]",
+# so that the dots do not trickle down to the labels in the web graph.
+colnames(network2) <- gsub(pattern = "\\.+",
                            replacement = " ",
                            x = colnames(network2))
+
 
 # visweb ------------------------------------------------------------------
 
 # Open a PDF graphic device with desired parameters
-pdf(file = "output/web_1.pdf",
+pdf(file = paste0("Output/web_graph_", format(Sys.time(), "%Y-%m-%d-%Hh%Mm%Ss"), ".pdf"),
     width = 20/2.54, 
     height = 10/2.54, 
     pointsize = 8)
@@ -37,7 +40,10 @@ par(oma = c(bottom = 0, left = 0, top = 0, right = 0),
     mar = c(bottom = 0, left = 0, top = 0, right = 0),
     lwd = 0.1)
 
-visweb(
+# Note that:
+# pred - Pollinators
+# prey - Plants
+plot_grid(
   web = network2,
   type = "nested",
   labsize = 2.7,
@@ -47,11 +53,11 @@ visweb(
   # Adjust axis labels side
   pred.axis.side = 3, 
   # Adjust position of axis labels:
-  pred.axis.pos = -7.7, # pred - Pollinators
-  prey.axis.pos = -3.3, # prey - Plants
+  pred.axis.pos = -7.9,
+  prey.axis.pos = -3.3,
   # Adjust position of axis titles:
-  pred.axis.title.line = -2.2, 
-  prey.axis.title.line = -1.3,
+  pred.axis.title.line = -2.5, 
+  prey.axis.title.line = -0.8,
   # increase axis title size if needed:
   axis.title.cex = 1, 
   # Adjusts the matrix left-right:
