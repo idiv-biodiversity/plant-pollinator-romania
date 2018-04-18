a57e843d3a9c039450f1180ddd0012cb458f4d52
valentinitnelav <stefan.valentin87@yahoo.com>
2018-04-18 15:41:22 +0200

    adapt to new datset

--- scripts/network_check_double_naming.r
+++ scripts/network_check_double_naming.r
@@ -1,25 +1,32 @@
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 # Script to test for duplicates in species naming (the network data).
-# Fixes "Eristalis tenax" issue.
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 library(data.table)
 
 # Read data
-network_orig <- read.csv("data/Rom_Trans.csv")
+network_orig <- read.csv("data/Rom_Trans_cor_new.csv")
 net2check <- network_orig
 
-# Check for duplicates in Plants
+
+# Simple check ------------------------------------------------------------
+
+# Check for duplicates in Plants.
+# integer(0) indicates no duplicates
 which(table(net2check$Plants) != 1)
 
 # Check for duplicates in column names (Pollinators)
 # First substitute dot/multiple dots with space
-names(net2check) <- gsub(pattern = "[.]+",
+names(net2check) <- gsub(pattern = "\\.+",
                          replacement = " ",
                          x = names(net2check))
 # which name appears more than once?
 which(table(names(net2check)) != 1) 
-# Eristalis tenax !
+# Previously was an issue with Eristalis tenax (data was corrected meanwhile)
+
+
+# Check & correct ---------------------------------------------------------
+# In case of no duplicates, stop here with the checking and run ro_bipartite_network.r
 
 # How are the duplicates looking like?
 names(network_orig)[names(network_orig) %like% "Eristalis.+tenax"]
@@ -32,7 +39,8 @@ network_orig[, .(Eristalis.tenax, Eristalis..tenax)] # check values
 network_orig[, Eristalis.tenax := Eristalis.tenax + Eristalis..tenax] # combine
 network_orig[, Eristalis..tenax := NULL] # delete column
 
+
 # Save file to be used further in ro_bipartite_network.r script 
 write.csv(network_orig, 
-          file = "data/Rom_Trans_cor.csv", 
+          file = "data/Rom_Trans_cor.csv", # change name if needed
           row.names = FALSE)
